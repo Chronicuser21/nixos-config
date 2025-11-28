ba73440eabb7b93bcbf49c87fa03599f64b59d1e
b <b@github.com>
2025-11-28 11:32:31 -0600

    Add system flake.nix and configuration.nix

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    apple-silicon.url = "github:nix-community/nixos-apple-silicon";
+   catppuccin.url = "github:catppuccin/nix"; 
+ };
+
+  outputs = inputs@{ self, nixpkgs, apple-silicon, catppuccin, ... }: 
+let
+system = "aarch64-linux";
+host = "asahi";
+username = "b";
+pkgs = import nixpkgs {
+inherit system;
+config = {
+allowUnfree = true;
+};
+};
+in 
+{
+nixosConfigurations = {
+${host} =
+nixpkgs.lib.nixosSystem rec {
+specialArgs = {
+inherit system;
+inherit inputs;
+inherit username;
+inherit host;
+};
+modules = [ inputs.apple-silicon.nixosModules.default 
+./hardware-configuration.nix
+./configuration.nix
+catppuccin.nixosModules.catppuccin
+];
+   };
+     };
+
+    };
+}
